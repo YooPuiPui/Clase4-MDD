177faf8be6d287fcabe1f71228939bcdf3c9f217
fran <panxihuaique2003@gmail.com>
2024-04-24 16:36:59 -0400

    Operadores-Aritmeticos.js

---
+++ Operadores-Aritmeticos.js
@@ -0,0 +1,37 @@
+//! Dado los siguiente valores numéricos:
+//!   const numero1 = 20;
+//!   const numero2 = 40;
+
+
+const numero1 = 20;
+const numero2 = 40;
+
+console.log("Numero 1 = 20");
+console.log("Numero 2 = 40");
+
+
+//? Suma
+
+const suma = numero1 + numero2;
+console.log("El resultado de la suma es: ", suma);
+
+
+//? Resta
+
+const resta = numero1 - numero2;
+console.log("El resultado de la resta es: ", resta);
+
+
+//? Multiplicacion
+
+const mul= numero1 * numero2;
+console.log("El resultado de la multiplicacion es: ", mul);
+
+
+//? Modulo
+
+const modulo = numero1 % numero2;
+console.log("El modulo entre N°1 y N°2 es: ", modulo);
+
+
+
