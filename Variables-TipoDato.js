ec102bac35cb7e93ddda3c841db2e8a5cd64f5ee
fran <panxihuaique2003@gmail.com>
2024-04-24 16:09:55 -0400

    ADD Variables-TipoDAto.js

---
+++ Variables-TipoDato.js
@@ -0,0 +1,22 @@
+//! 1. Declara una variable llamada `nombre` y asígnale un valor de tipo `string`.
+//! 2. Declara una variable llamada edad y asígnale un valor de tipo `number`.
+//! 3. Declara una variable llamada `puedoConducir` y asígnale un valor de tipo `boolean` .
+//! 4. Para cada variable se debe imprimir por consola el resultado de cada variable declarada.
+//! 5. Para cada variable se debe imprimir el tipo de dato que es por consola.
+
+//? Declarar variables 
+
+let nombre = "Francisca Huaique";
+let edad = 20;
+let puedoConducir = true;
+
+//? Imprimir por consola
+
+console.log("Nombre: ", nombre);
+console.log("Tipo de dato de nombre: ", typeof nombre);
+
+console.log("Edad: ", edad);
+console.log("Tipo de dato de edad: ", typeof edad);
+
+console.log("Puede conducir?: ", puedoConducir);
+console.log("Tipo de dato de puede conducir: ", typeof puedoConducir);
