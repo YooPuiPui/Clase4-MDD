cebf3902ff7f999cfdaccfc08b11c57dcb71186a
fran <panxihuaique2003@gmail.com>
2024-04-25 21:20:17 -0400

    add Objetos.js

---
+++ Objetos.js
@@ -0,0 +1,30 @@
+
+
+const persona = {
+
+    nombre: "francisca",
+    edad: 20,
+    genero: "femenino"
+
+}
+
+console.log(persona);
+
+
+const caja ={
+
+    cuadernos: 3,
+    lapices: 10,
+    papel: "hp, equalit, ovella",
+    fotografias: 20,
+    estado: true,
+}
+
+console.log(caja);
+console.log(caja.cuadernos, "es tipo",  typeof caja.cuadernos);
+console.log(caja.lapices, "es tipo",  typeof caja.lapices);
+console.log(caja.papel, "es tipo",  typeof caja.papel);
+console.log(caja.fotografias, "es tipo", typeof caja.fotografias);
+console.log(caja.estado, "es tipo",  typeof caja.estado);
+
+
